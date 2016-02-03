286145483ab61380ef53ea57adc940feacbc968a
Ben Liang <bliang@cliquesads.com>
2016-02-03 00:58:33 -0500

    BF: Async loaded scripts wouldn't load after Ajax success

--- getClientLoader.js
+++ getClientLoader.js
@@ -20,6 +20,35 @@ module.exports = function(exchangeHostname, exchangeSecureHostname, pubPath){
             pub_path: '%s'
         };
 
+        var copyAttributes = function(oldNode, newNode){
+            for (var i=0; i < oldNode.attributes.length; i++){
+                var attr = oldNode.attributes[i];
+                newNode.setAttribute(attr.name, attr.value);
+            }
+        };
+
+        var replaceScripts = function(parent){
+            for (var j=0; j < parent.children.length; j++){
+                var thisNode = parent.children[j];
+                if (thisNode.nodeName === 'SCRIPT'){
+                    if (thisNode.attributes.src){
+                        var nextSibling = thisNode.nextSibling;
+                        parent.removeChild(thisNode);
+                        // now create new element
+                        var new_s = document.createElement("script");
+                        copyAttributes(thisNode, new_s);
+                        parent.insertBefore(new_s, nextSibling);
+                    } else {
+                        // #sorrynotsorry
+                        eval(thisNode.innerText);
+                    }
+                }
+                if (thisNode.children.length > 0){
+                    replaceScripts(thisNode);
+                }
+            }
+        };
+
         return {
             init: function (options) {
                 _args.pid = options.pid;
@@ -32,7 +61,11 @@ module.exports = function(exchangeHostname, exchangeSecureHostname, pubPath){
             main: function (){
                 var xmlHttp = new XMLHttpRequest();
                 xmlHttp.onreadystatechange = function() {
-                    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) document.write(xmlHttp.responseText);
+                    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+                        var el = document.getElementById('cloader-' + _args.pid);
+                        el.innerHTML = xmlHttp.responseText;
+                        replaceScripts(el);
+                    }
                 };
                 xmlHttp.open("GET", _args.url, true); // true for asynchronous
                 xmlHttp.send(null);
